49957d66b8d0844d059f3082f25841bed9d10f55
sushant patra <sushantapatra.sk@gmail.com>
2026-02-14 17:09:25 +0530

    Updated file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const mongoURL = "mongodb://127.0.0.1:27017/wonderlust";
 const path = require("path");
+const methodOverride = require("method-override")
 
 async function main(){
     await mongoose.connect(mongoURL);
@@ -12,6 +13,7 @@ async function main(){
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 //index route
 app.get("/listings",async (req,res)=>{
@@ -39,6 +41,19 @@ app.post("/listings",async(req,res)=>{
     res.redirect("/listings");
 })
 
+//Edit route
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    let list = await Listing.findById(id);
+    res.render("listings/edit.ejs",{list});    
+})
+
+//Update route
+app.put(" m /listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.Listing});
+    redirect("/listings");
+})
 
 // app.get("/testListing",async (req,res)=>{
 //     let sampleListing = new Listing({
